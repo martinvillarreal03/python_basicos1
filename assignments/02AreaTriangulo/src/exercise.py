3eacd95ebe1b15ea8415830ba5fc574883717fa4
martinvillarreal03 <88672283+martinvillarreal03@users.noreply.github.com>
2021-08-12 10:49:38 -0500

    Update exercise.py

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,6 +1,39 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+   
+"""
+Area Triangulo
+
+Analisis
+
+Datos de Entrada:
+Base float
+Altura float
+
+Datos de Salida:
+Mensajes para ingresar los datos de entrada
+Desplegar el area como se solicita
+
+Procesos:
+Calcular (b * a) / 2
+
+"""
+
+#Leer datos (base * altura)
+
+b = float(input('Cual es la base: '))
+a = float(input('Cual la altura: '))
+
+
+#Calcular datos (b * a) / 2
+
+area = (b * a) / 2
+
+#Desplegar Datos
+
+print('Tu area es: ', area) 
+
+
 
 
 if __name__ == '__main__':
